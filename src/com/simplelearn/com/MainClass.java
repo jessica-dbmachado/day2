9574bae6335cf35710bbe29581908a1ae3fe5721
Jessica Barcelos <j_machado@dell.com>
2021-02-11 17:27:39 -0300

    AddString and comment

--- src/com/simplelearn/com/MainClass.java
+++ src/com/simplelearn/com/MainClass.java
@@ -15,6 +15,13 @@ public class MainClass {
 		honda.addGallons(2);
 		System.out.println(honda.getAmtGasoline());
 		//just a comment
+		
+		//array
+		String[] testarray= {"1st elemnt", "2nd element","3rd element","4th element","5th element"};
+		for (int i = 0; i <= 2; i++) {
+			  System.out.println("i= " + i + " Array content: "+testarray[i]+ "\n");
+			  //test123
+			}
 	}
 
 }
